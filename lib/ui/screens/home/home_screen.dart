5e708c6e681518f49aee46012c8cfe814b8715be
Ly Dara <dara.ly@student.cadt.edu.kh>
2026-03-19 23:42:39 +0700

    BLA-203 - Ride Preferences Global State

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:blabla/model/ride_pref/ride_pref.dart';
-import 'package:blabla/services/ride_prefs_service.dart';
 import 'package:blabla/ui/states/ride_preference_state.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
